59e8b0a15c432aa5fc76407ca2046d58b49c1573
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2021-11-02 19:20:53 -0500

    Add files via upload

---
+++ Menu Lateral desplegable/main.py
@@ -0,0 +1,103 @@
+# @autor: Magno Efren
+# Youtube: https://www.youtube.com/c/MagnoEfren
+
+import sys
+from menu import *
+from PyQt5 import QtCore
+from PyQt5.QtCore import QPropertyAnimation
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class MiApp(QtWidgets.QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.ui = Ui_MainWindow() 
+		self.ui.setupUi(self)
+
+		#eliminar barra y de titulo - opacidad
+		self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+		self.setWindowOpacity(1)
+
+		#SizeGrip
+		self.gripSize = 10
+		self.grip = QtWidgets.QSizeGrip(self)
+		self.grip.resize(self.gripSize, self.gripSize)
+
+		# mover ventana
+		self.ui.frame_superior.mouseMoveEvent = self.mover_ventana
+
+		#acceder a las paginas
+		self.ui.bt_inicio.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page))			
+		self.ui.bt_uno.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_uno))
+		self.ui.bt_dos.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_dos))	
+		self.ui.bt_tres.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_tres))
+		self.ui.bt_cuatro.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_cuatro))			
+		self.ui.bt_cinco.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_cinco))	
+
+		#control barra de titulos
+		self.ui.bt_minimizar.clicked.connect(self.control_bt_minimizar)		
+		self.ui.bt_restaurar.clicked.connect(self.control_bt_normal)
+		self.ui.bt_maximizar.clicked.connect(self.control_bt_maximizar)
+		self.ui.bt_cerrar.clicked.connect(lambda: self.close())
+
+		self.ui.bt_restaurar.hide()
+
+		#menu lateral
+		self.ui.bt_menu.clicked.connect(self.mover_menu)
+
+	def control_bt_minimizar(self):
+		self.showMinimized()		
+
+	def  control_bt_normal(self): 
+		self.showNormal()		
+		self.ui.bt_restaurar.hide()
+		self.ui.bt_maximizar.show()
+
+	def  control_bt_maximizar(self): 
+		self.showMaximized()
+		self.ui.bt_maximizar.hide()
+		self.ui.bt_restaurar.show()
+
+	def mover_menu(self):
+		if True:			
+			width = self.ui.frame_lateral.width()
+			normal = 0
+			if width==0:
+				extender = 200
+			else:
+				extender = normal
+			self.animacion = QPropertyAnimation(self.ui.frame_lateral, b'minimumWidth')
+			self.animacion.setDuration(300)
+			self.animacion.setStartValue(width)
+			self.animacion.setEndValue(extender)
+			self.animacion.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+			self.animacion.start()
+
+	## SizeGrip
+	def resizeEvent(self, event):
+		rect = self.rect()
+		self.grip.move(rect.right() - self.gripSize, rect.bottom() - self.gripSize)
+
+	## mover ventana
+	def mousePressEvent(self, event):
+		self.clickPosition = event.globalPos()
+
+	def mover_ventana(self, event):
+		if self.isMaximized() == False:			
+			if event.buttons() == QtCore.Qt.LeftButton:
+				self.move(self.pos() + event.globalPos() - self.clickPosition)
+				self.clickPosition = event.globalPos()
+				event.accept()
+
+		if event.globalPos().y() <=20:
+			self.showMaximized()
+		else:
+			self.showNormal()
+
+
+if __name__ == "__main__":
+     app = QtWidgets.QApplication(sys.argv)
+     mi_app = MiApp()
+     mi_app.show()
+     sys.exit(app.exec_())	
+
+
