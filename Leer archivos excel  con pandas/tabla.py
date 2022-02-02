ecc64ee8a471407470e1d55eba61562944ab7360
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2022-02-02 16:59:41 -0500

    Update tabla.py

--- Leer archivos excel  con pandas/tabla.py
+++ Leer archivos excel  con pandas/tabla.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'tabla.ui'
-#
 # Created by: PyQt5 UI code generator 5.12.3
-#
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
