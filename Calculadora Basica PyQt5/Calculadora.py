5a963a8eb33b6c02f21e923c676141e67ea3aa5c
Magno Efren <58579707+MagnoEfren@users.noreply.github.com>
2024-08-30 19:07:00 -0500

    Update Calculadora.py

--- Calculadora Basica PyQt5/Calculadora.py
+++ Calculadora Basica PyQt5/Calculadora.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'calculadora.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
+# Form implementation generated from reading ui file 'calculadora.ui'#
+# Created by: PyQt5 UI code generator 5.9.2#
 # WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Form(object):
